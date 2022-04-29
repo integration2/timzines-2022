5a2ed23d13be0099db82790a222bb364ded39c23
Benjamin Brouillette <2165920@csfoy.ca>
2022-04-29 10:02:21 -0400

    allo

--- BenjaminBrouillette/app.js
+++ BenjaminBrouillette/app.js
@@ -1,10 +1,24 @@
 const jeu = {
-
+    let b = document.querySelectorAll('.petite_blague');
     blague: function () {
-        document.querySelectorAll('.petite_blague').removeAttribute('hidden');
+
+    }
+};
+
+demo = {
+    intClics: 0,
+    tagSpanNombreClics: document.querySelector('.clics'),
+    ajouterClic: function (objetEvent) {
+        if (objetEvent.keyCode == 32) {
+            this.intClics++;
+            this.afficherClics();
+        }
     }
 };
 
 document.querySelectorAll('.petite_blague').addEventListener('keydown', function() {
    jeu.blague();
+});
+document.querySelector('body').addEventListener('keyup', function (objEvenement) {
+    demo.ajouterClic(objEvenement);
 });
